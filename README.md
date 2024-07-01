c36f4a5108e68e2c2af213840b0c7e3850e448ba
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-07-01 20:12:06 +0530

    restart again

---
+++ README.md
@@ -0,0 +1,13 @@
+# HorridAPI
+
+``` 
+pip install HorridAPI
+```
+
+
+# Llama Ai
+```
+from HorridAPI import api
+ai = api.llama("who are you")
+print(ai)
+```
---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", encoding="utf8") as readme:
+    long_desc = readme.read()
+    
+
+
+# Setting up
+setup(
+    name="HorridAPI",
+    version='0.2',
+    author="Horrid",
+    author_email="narutomalayalam@gmail.com",
+    description="Horrid api toolz",
+    long_description_content_type="text/markdown",
+    long_description=long_desc,
+    packages=find_packages(),
+    license="MIT",
+    url="https://github.com/Mishel-Tg/HorridAPI",
+    download_url="https://github.com/Mishel-Tg/HorridAPI/blob/main/README.md",
+    install_requires=["requests"],
+    keywords=['python', "HorridAPI","mrz_bots", "telegram", "WhatsAppbot"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    
+    project_urls={
+        "Tracker": "https://github.com/Mishel-Tg/HorridAPI/issues",
+        "Community": "https://t.me/XBOTSUPPORTS",
+        "Source": "https://github.com/Mishel-Tg/HorridAPI",
+    },
+    python_requires="~=3.7",
+)
