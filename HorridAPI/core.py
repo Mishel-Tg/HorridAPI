a82a1197c6ed134c00da34315d65a3d9e5a3fcfb
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-07-23 15:27:15 +0530

    Update core.py

--- HorridAPI/core.py
+++ HorridAPI/core.py
@@ -4,7 +4,15 @@ from telegraph import upload_file
 
 class Core:
     """HorridAPI Core"""    
-    
+
+    def elon_musk (url: str):                
+        prompt = "You are elon musk you ceo of tesla"
+        return prompt
+        
+    def assistant (url: str):                
+        prompt = "You are helpfull assistant"
+        return prompt
+        
     def upload(url: str):                
         out = upload_file(url))
         return out
