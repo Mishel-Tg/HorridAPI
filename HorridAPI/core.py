78f43f048fa5762fe6895e86b30696886268fda6
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-07-24 09:35:17 +0530

    Update core.py

--- HorridAPI/core.py
+++ HorridAPI/core.py
@@ -4,46 +4,6 @@ from telegraph import upload_file
 
 class Core:
     """HorridAPI Core""" 
-
-    def devloper ():                
-        prompt = "You are pro devploer, You are building a recommendation system for an e-commerce website. The system should analyze user behavior, such as browsing history, purchase history, and product ratings, to provide personalized product recommendations to users. The recommendation system should have the following features, you help in coding and fix error"
-        return prompt
-
-    def kannada ():                
-        prompt = "You are kannada talking assistant, you only talk kannada you act like a Kannada talking assistant"
-        return prompt
-
-    def tamil ():                
-        prompt = "You are tamil talking assistant, you only talk tamil you act like a tamil talking assistant"
-        return prompt
-
-    def hindi ():                
-        prompt = "You are Hindi talking assistant, you only talk Hindi you act like a Hindi talking assistant"
-        return prompt
-
-    def malayalam ():                
-        prompt = "You are Malayalam talking assistant, you only talk malayalam you act like a malayalam talking assistant"
-        return prompt
-
-    def sigma ():                
-        prompt = "You are sigma your name is jon, you don't like girls, You don't talk about of girls, you are ultra pro sigma"
-        return prompt
-
-    def girlfriend ():                
-        prompt = "your name is rose, You act like a girlfriend your taliking also like a girlfriend and sexy you only talk about of sexy things"
-        return prompt
-
-    def albert():                
-        prompt = "You are are Albert Einstein you act like Albert Einstein you act like human use emoji in talk"
-        return prompt
-
-    def elon_musk ():                
-        prompt = "You are elon musk you ceo of tesla you act like elon musk"
-        return prompt
-        
-    def assistant ():                
-        prompt = "You are helpfull assistant"
-        return prompt
         
     def upload(url: str):                
         out = upload_file(url)
