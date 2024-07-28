3ef7065ca9b647de075883ff39ae054b37754eb3
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-07-28 16:21:17 +0530

    Update AiGenerativeContent.py

--- HorridAPI/AiGenerativeContent.py
+++ HorridAPI/AiGenerativeContent.py
@@ -5,11 +5,18 @@ class AiGenerativeContent:
     def HorridAPI(query, model):
         url = "https://horrid-api.onrender.com/ai"
         out = f"{url}?model={model}"
-        response = requests.post(out, json=query)
-        return response.text
+        response = requests.post(out, json=query)        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.text            
         
     def Content(query, model):
         url = "https://horrid-api.onrender.com/ai"
         out = f"{url}?query={query}&model={model}"
-        response = requests.post(out)
-        return response.text
+        response = requests.post(out)        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.text
+        
