acc4a6bdbf24587faa0f678f1cdf6fda1570ca24
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-08-23 21:33:59 +0530

    Update core.py

--- HorridAPI/core.py
+++ HorridAPI/core.py
@@ -1,3 +1,44 @@
+gemini = "1"
+gemma = "2"
+gemini_pro = 3
+gemmaa = 4
+gpt = 5
+
+
+class models:
+    """HorridAPI Model""" 
+
+    def gemma():                
+        out = "2"
+        return out
+
+    def gemma_2():                
+        out = "4"
+        return out
+
+    def gemini_pro():                
+        out = "3"
+        return out
+    
+    def gpt():                
+        out = "5"
+        return out
+        
+    def gemini():                
+        out = "1"
+        return out
+
+
+
+
+
+
+
+
+
+
+
+
 from telegraph import upload_file
 
 class Core:
