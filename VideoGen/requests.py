66360aa81916a90ee4ba1c41e178174ae9850686
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-09-20 18:59:12 +0530

    Create requests.py

---
+++ VideoGen/requests.py
@@ -0,0 +1,13 @@
+# made by @Mrz_bots
+
+import requests
+import os
+
+class Requests:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_video(self, prompt):
+        url = f"https://horrid-api-yihb.onrender.com/txt2vid?prompt={prompt}&api_key={self.api_key}"
+        response = requests.get(url)
+        return response.content
