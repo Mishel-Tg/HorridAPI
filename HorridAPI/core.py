652d9bdc82661657db62f9fb7164a0fc2a472697
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-09-28 13:03:14 +0530

    Update core.py

--- HorridAPI/core.py
+++ HorridAPI/core.py
@@ -33,8 +33,6 @@ from telegraph import upload_file
 class Core:
     """HorridAPI Core""" 
         
-    def upload(url: str):                
-        out = upload_file(url)
-        put = f"https://telegra.ph/{out[0]}"
-        return put
+    def upload(url: str):                      
+        return 'none'
           
