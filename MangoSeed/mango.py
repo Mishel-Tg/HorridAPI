3aec0d6cb48452d5b5e483f5a5017dbac36c922d
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-10-08 19:00:45 +0530

    No need

--- MangoSeed/mango.py
+++ MangoSeed/mango.py
@@ -7,22 +7,11 @@ class Mseed:
     def __init__(self, mongo_url):
         return None
     
-    def model(self, model):
-        """
-        Get the model number from the model name.
-
-        Args:
-            model (str): The name of the model.
-
-        Returns:
-            int: The model number.
-        """
-        if model not in self.models:
-            return "Invalid model. You Can Get model here https://horrid-api-yihb.onrender.com/ai."
-        return self.models[model]
+    def model(self, model):        
+        return None
 
     def generate(self, system, prompt, user_id, model):        
-        return {"result": "removed temporary"}
+        return None
 
     def delete_user_messages(self, user_id):
         return None
