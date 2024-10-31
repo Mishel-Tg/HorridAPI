3ce3820b712e33ea58efa236bb01b97e812edbcb
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-10-31 19:33:52 +0530

    Now fixed completely all errors

--- HorridAPI/__init__.py
+++ HorridAPI/__init__.py
@@ -2,8 +2,8 @@ from .api import *
 from .Mango import *
 from .AiGenerativeContent import *
 from .MrzSong import *
-from Async import *
+from .Async import *
 
-__version__ = "1.0.44"
+__version__ = "1.0.45"
 
 __all__ = ["api"]
