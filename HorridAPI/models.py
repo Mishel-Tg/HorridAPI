03d9493503fc7b7fd6b977f2f1e82dd836ae048f
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-10-31 14:50:35 +0530

    Update models.py

--- HorridAPI/models.py
+++ HorridAPI/models.py
@@ -1,8 +1,21 @@
-# our model list 
+# ethinda pakaram vera add akki marukatha erukan vendi pinne eth use akm 
+# now no need
+# gemini = "1"
+# gemma = "2"
+# gemini_pro = 3
+# gemmaa = 4
+# gpt = 5
+# gpt_o = 6
 
-gemini = "1"
-gemma = "2"
-gemini_pro = 3
-gemmaa = 4
-gpt = 5
-gpt_o = 6
+
+
+mango = {
+    "gemini": 1,
+    "gemma": 2,
+    "gemini-pro": 3,
+    "gemma-2": 4,
+    "gpt-3.5": 5,
+    "gpt-4o-mini": 6,
+    "gpt-4o-mini-2": 7,
+    "llama3-70b": 8
+}
