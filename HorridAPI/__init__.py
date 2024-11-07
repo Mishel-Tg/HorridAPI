ce9f777ba1e0379cd1bd630df35e75ba108d5dbd
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-11-07 19:21:17 +0530

    Update __init__.py

--- HorridAPI/__init__.py
+++ HorridAPI/__init__.py
@@ -2,8 +2,9 @@ from .api import *
 from .Mango import *
 from .AiGenerativeContent import *
 from .MrzSong import *
+from .Llava import LLVA
 from .Async import *
 
-__version__ = "1.0.61"
+__version__ = "1.0.62"
 
 __all__ = ["api"]
