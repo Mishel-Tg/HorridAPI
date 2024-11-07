e35d4614e141de24c64caa908c9f5267ff3d2ab7
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-11-07 19:20:33 +0530

    Create Llava.py

---
+++ HorridAPI/Llava.py
@@ -0,0 +1,16 @@
+from VideoGen.llava import Requests
+
+class LLVA:
+    def __init__(self, prompt=None, api_key=None):
+        if not prompt:
+          raise ValueError("You Prompt was empty")
+        if not api_key:
+          raise ValueError("Where the api key you can get api key here https://t.me/XBOTSUPPORTS")
+        self.requests = Requests(api_key)
+        self.prompt = prompt
+
+    def generate_video(self):
+        video_content = self.requests.get_video(self.prompt)
+        with open("video.mp4", "wb") as f:
+            f.write(video_content)
+        return "video.mp4"
